9f1b77ce9214ed7a0b8e40e86ef735e8e588bedf
智 李 <lizhi1977@hotmail.com>
2016-09-24 20:34:16 +0900

    update the images.

--- images/create_fig.bat
+++ images/create_fig.bat
@@ -1,86 +1,86 @@
-convert -size 48x48 canvas:none -fill transparent -stroke orange -draw "circle 24,24 24,45 circle 24,24 24,42 line 4,4 16,16 line 44,44 32,32 line 4,44 16,32 line 44,4 32,16" cross_orange.png
-convert -size 48x48 canvas:none -fill transparent -stroke red -draw "circle 24,24 24,45 circle 24,24 24,42 line 4,4 16,16 line 44,44 32,32 line 4,44 16,32 line 44,4 32,16" cross_red.png
-convert -size 48x48 canvas:none transparent.png
+magick -size 48x48 canvas:none -fill transparent -stroke orange -draw "circle 24,24 24,45 circle 24,24 24,42 line 4,4 16,16 line 44,44 32,32 line 4,44 16,32 line 44,4 32,16" cross_orange.png
+magick -size 48x48 canvas:none -fill transparent -stroke red -draw "circle 24,24 24,45 circle 24,24 24,42 line 4,4 16,16 line 44,44 32,32 line 4,44 16,32 line 44,4 32,16" cross_red.png
+magick -size 48x48 canvas:none transparent.png
 
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'A'" cross_red_A.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'B'" cross_red_B.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'C'" cross_red_C.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'D'" cross_red_D.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'E'" cross_red_E.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'F'" cross_red_F.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'G'" cross_red_G.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'H'" cross_red_H.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'I'" cross_red_I.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'J'" cross_red_J.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'K'" cross_red_K.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'L'" cross_red_L.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'M'" cross_red_M.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'N'" cross_red_N.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'O'" cross_red_O.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'P'" cross_red_P.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'Q'" cross_red_Q.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'R'" cross_red_R.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'S'" cross_red_S.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'T'" cross_red_T.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'U'" cross_red_U.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'V'" cross_red_V.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'W'" cross_red_W.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'X'" cross_red_X.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'Y'" cross_red_Y.png
-convert transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'Z'" cross_red_Z.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'A'" cross_orange_A.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'B'" cross_orange_B.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'C'" cross_orange_C.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'D'" cross_orange_D.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'E'" cross_orange_E.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'F'" cross_orange_F.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'G'" cross_orange_G.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'H'" cross_orange_H.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'I'" cross_orange_I.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'J'" cross_orange_J.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'K'" cross_orange_K.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'L'" cross_orange_L.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'M'" cross_orange_M.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'N'" cross_orange_N.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'O'" cross_orange_O.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'P'" cross_orange_P.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'Q'" cross_orange_Q.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'R'" cross_orange_R.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'S'" cross_orange_S.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'T'" cross_orange_T.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'U'" cross_orange_U.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'V'" cross_orange_V.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'W'" cross_orange_W.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'X'" cross_orange_X.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'Y'" cross_orange_Y.png
-convert cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 24 -gamma 2.2 -draw "text 0,2 'Z'" cross_orange_Z.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'A'" cross_red_A.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'B'" cross_red_B.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'C'" cross_red_C.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'D'" cross_red_D.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'E'" cross_red_E.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'F'" cross_red_F.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'G'" cross_red_G.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'H'" cross_red_H.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'I'" cross_red_I.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'J'" cross_red_J.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'K'" cross_red_K.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'L'" cross_red_L.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'M'" cross_red_M.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'N'" cross_red_N.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'O'" cross_red_O.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'P'" cross_red_P.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'Q'" cross_red_Q.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'R'" cross_red_R.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'S'" cross_red_S.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'T'" cross_red_T.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'U'" cross_red_U.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'V'" cross_red_V.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'W'" cross_red_W.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'X'" cross_red_X.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'Y'" cross_red_Y.png
+magick transparent.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'Z'" cross_red_Z.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'A'" cross_orange_A.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'B'" cross_orange_B.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'C'" cross_orange_C.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'D'" cross_orange_D.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'E'" cross_orange_E.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'F'" cross_orange_F.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'G'" cross_orange_G.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'H'" cross_orange_H.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'I'" cross_orange_I.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'J'" cross_orange_J.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'K'" cross_orange_K.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'L'" cross_orange_L.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'M'" cross_orange_M.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'N'" cross_orange_N.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'O'" cross_orange_O.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'P'" cross_orange_P.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'Q'" cross_orange_Q.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'R'" cross_orange_R.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'S'" cross_orange_S.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'T'" cross_orange_T.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'U'" cross_orange_U.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'V'" cross_orange_V.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'W'" cross_orange_W.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'X'" cross_orange_X.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'Y'" cross_orange_Y.png
+magick cross_orange.png -fill orange -stroke orange -gravity center -font georgia -pointsize 20 -gamma 2.2 -draw "text 0,2 'Z'" cross_orange_Z.png
 
-convert cross_orange_A.png cross_red_A.png +append cross_A.png
-convert cross_orange_B.png cross_red_B.png +append cross_B.png
-convert cross_orange_C.png cross_red_C.png +append cross_C.png
-convert cross_orange_D.png cross_red_D.png +append cross_D.png
-convert cross_orange_E.png cross_red_E.png +append cross_E.png
-convert cross_orange_F.png cross_red_F.png +append cross_F.png
-convert cross_orange_G.png cross_red_G.png +append cross_G.png
-convert cross_orange_H.png cross_red_H.png +append cross_H.png
-convert cross_orange_I.png cross_red_I.png +append cross_I.png
-convert cross_orange_J.png cross_red_J.png +append cross_J.png
-convert cross_orange_K.png cross_red_K.png +append cross_K.png
-convert cross_orange_L.png cross_red_L.png +append cross_L.png
-convert cross_orange_M.png cross_red_M.png +append cross_M.png
-convert cross_orange_N.png cross_red_N.png +append cross_N.png
-convert cross_orange_O.png cross_red_O.png +append cross_O.png
-convert cross_orange_P.png cross_red_P.png +append cross_P.png
-convert cross_orange_Q.png cross_red_Q.png +append cross_Q.png
-convert cross_orange_R.png cross_red_R.png +append cross_R.png
-convert cross_orange_S.png cross_red_S.png +append cross_S.png
-convert cross_orange_T.png cross_red_T.png +append cross_T.png
-convert cross_orange_U.png cross_red_U.png +append cross_U.png
-convert cross_orange_V.png cross_red_V.png +append cross_V.png
-convert cross_orange_W.png cross_red_W.png +append cross_W.png
-convert cross_orange_X.png cross_red_X.png +append cross_X.png
-convert cross_orange_Y.png cross_red_Y.png +append cross_Y.png
-convert cross_orange_Z.png cross_red_Z.png +append cross_Z.png
+magick cross_orange_A.png cross_red_A.png +append cross_A.png
+magick cross_orange_B.png cross_red_B.png +append cross_B.png
+magick cross_orange_C.png cross_red_C.png +append cross_C.png
+magick cross_orange_D.png cross_red_D.png +append cross_D.png
+magick cross_orange_E.png cross_red_E.png +append cross_E.png
+magick cross_orange_F.png cross_red_F.png +append cross_F.png
+magick cross_orange_G.png cross_red_G.png +append cross_G.png
+magick cross_orange_H.png cross_red_H.png +append cross_H.png
+magick cross_orange_I.png cross_red_I.png +append cross_I.png
+magick cross_orange_J.png cross_red_J.png +append cross_J.png
+magick cross_orange_K.png cross_red_K.png +append cross_K.png
+magick cross_orange_L.png cross_red_L.png +append cross_L.png
+magick cross_orange_M.png cross_red_M.png +append cross_M.png
+magick cross_orange_N.png cross_red_N.png +append cross_N.png
+magick cross_orange_O.png cross_red_O.png +append cross_O.png
+magick cross_orange_P.png cross_red_P.png +append cross_P.png
+magick cross_orange_Q.png cross_red_Q.png +append cross_Q.png
+magick cross_orange_R.png cross_red_R.png +append cross_R.png
+magick cross_orange_S.png cross_red_S.png +append cross_S.png
+magick cross_orange_T.png cross_red_T.png +append cross_T.png
+magick cross_orange_U.png cross_red_U.png +append cross_U.png
+magick cross_orange_V.png cross_red_V.png +append cross_V.png
+magick cross_orange_W.png cross_red_W.png +append cross_W.png
+magick cross_orange_X.png cross_red_X.png +append cross_X.png
+magick cross_orange_Y.png cross_red_Y.png +append cross_Y.png
+magick cross_orange_Z.png cross_red_Z.png +append cross_Z.png
 
-convert cross_[A-Z].png -append crosses.png
+magick cross_[A-Z].png -append crosses.png
 
